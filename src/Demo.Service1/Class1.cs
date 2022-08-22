4c61303b2772dfe8305b8e0e9258294ad6593311
Peter Chamley <peter@chamley.name>
2022-08-22 11:56:36 +0100

    Push changes to 2 repos from VS.

--- src/Demo.Service1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Demo.Service1
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Demo.Service1/Service1.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Common;
+
+namespace Demo.Service1
+{
+    public class Service1 : IService
+    {
+        public string ValuePassedIn { get ; set ; }
+
+        public string DoSomeWork()
+        {
+            return $"This is what is the the property 'ValuePassedIn': ${ValuePassedIn}";
+        }
+    }
+}
