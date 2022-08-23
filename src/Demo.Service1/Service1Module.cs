7d5c38c540d36d562f8d215262de333a7c33ea18
Peter Chamley <peter@chamley.name>
2022-08-23 15:31:21 +0100

    Add AutoFac to Sub Modules and Demo Site. Update Common Service - See Rippple afface

---
+++ src/Demo.Service1/Service1Module.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Demo.Common;
+
+namespace Demo.Service1
+{
+    public class Service1Module : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Service1>().As<IService>();
+            builder.RegisterType<Service1abc>().As<IService1abc>();
+        }
+    }
+}
